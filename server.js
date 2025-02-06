a4437bfbfe05c7d745d54ea9fe7ff3883dcee384
thexys <johnsaberon2000@gmail.com>
2025-02-06 17:27:25 +0800

    hello

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { exec } = require("child_process");
+const {exec} = require("child_process");
 
 const app = express();
 const PORT = 3000;
@@ -7,16 +7,18 @@ const PORT = 3000;
 app.use(express.static(__dirname));
 
 app.get("/open-rustdesk", (req, res) => {
-  exec('start "" "C:\\Program Files\\RustDesk\\RustDesk.exe"', (error) => {
-    if (error) {
-      res.status(500).send("Error opening RustDesk");
-      return;
-    } else {
-      res.send("RustDesk opened successfully");
-    }
-  });
+	exec('start "" "C:\\Program Files\\RustDesk\\RustDesk.exe"', (error) => {
+		if (error) {
+			res.status(500).send("Error opening RustDesk");
+			return;
+		} else {
+			res.send("RustDesk opened successfully");
+		}
+	});
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Hellooo
